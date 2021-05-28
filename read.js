45bbb130480ff7ea96da4c35ebb17f4060ea1078
Juan Pablo <c137dts@gmail.com>
2021-05-28 15:17:47 -0500

    Convirtiendo todo en una clase para exportar de mejor manera

--- read.js
+++ read.js
@@ -6,39 +6,45 @@
 const prompt = require("prompt");
 const fs = require("fs");
 
-// Regresa el contenido del archivo de forma sincrona
-function leer(ruta, cb) {
-  return fs.readFileSync(ruta, "utf8");
-}
-
-// Muestra el conteido del archivo el parametro file es la ruta del archivo
-function open(file) {
-  let response = leer(file);
-  console.log("\n", response);
-  console.log("\nEl proceso ha terminado...");
-}
-
-// Imprime los mensajes en pantalla para que el ususario decida si quiere ver o no el continido del archivo
-prompt.start();
-console.log(
-  "Presiona la tecla Y si deseas abrir el archivo o presiona enter para omitir: "
-);
-prompt.get("open", (err, result) => {
-  if (err) {
-    console.error(err);
-    return;
+class reader {
+  // Regresa el contenido del archivo de forma sincrona
+  leer(ruta, cb) {
+    return fs.readFileSync(ruta, "utf8");
   }
-  if (result.open.toLowerCase() === "y") {
-    console.log("Escriba la ruta del archivo ");
-    prompt.get("file", (err, result) => {
+
+  // Muestra el conteido del archivo el parametro file es la ruta del archivo
+  open(file) {
+    let response = this.leer(file);
+    console.log("\n", response);
+    console.log("\nEl proceso ha terminado...");
+  }
+
+  interface() {
+    // Imprime los mensajes en pantalla para que el ususario decida si quiere ver o no el continido del archivo
+    prompt.start();
+    console.log(
+      "Presiona la tecla Y si deseas abrir el archivo o presiona enter para omitir: "
+    );
+    prompt.get("open", (err, result) => {
       if (err) {
         console.error(err);
         return;
       }
-      open(result.file);
+      if (result.open.toLowerCase() === "y") {
+        console.log("Escriba la ruta del archivo ");
+        prompt.get("file", (err, result) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          this.open(result.file);
+        });
+      } else {
+        console.log("\nEl proceso ha finalizado exitosamente");
+      }
     });
-  } else {
-    console.log("\nEl proceso ha finalizado exitosamente");
   }
-});
+}
 
+// let prueba = new reader()
+// prueba.interface()
