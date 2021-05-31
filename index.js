51acc77b5ed5b4ef4c202f297bc3597f7a63520e
Juan Pablo <c137dts@gmail.com>
2021-05-31 07:42:43 -0500

    agregando comentarios

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ const { SSL_OP_NETSCAPE_REUSE_CIPHER_CHANGE_BUG } = require("constants");
 let file_name = "index.js";
 let dir_files = [];
 function ls(directory, cb) {
+    // Muestra los archivos y directorios que tenemos en la ruta indicada
   exec(`dir ${directory} /b`, (err, stdout, stderr) => {
     if (err) {
       console.error(err.message);
     } else {
+        // Convierte cada archivo y directorio en un elemento de un array
       dir_files = stdout.split("\n");
       cb(dir_files);
     }
@@ -23,16 +25,20 @@ function ls(directory, cb) {
 }
 
 function search(files) {
+    // Busca el la lista de archivos del directorio un archivo que coincida con el nombre de archivo que proporcionamos.
   let maybe = files.filter((file) => file == `${file_name}\r`);
   if (maybe[0]) {
     console.log("El archivo se encuentra en este directorio");
+    // Dispara la consola interactiva para elegir si queremos abrir el archivo
     reader.interface();
   } else {
+      // Si no se encuentra una coincidencia se buscara un archivo similar
     similars(files);
   }
 }
 
 function similars(files) {
+    // Busca una palabra similar al archivo que buscamos usando levenshtein
   let suggestion = files.find((file) => levenshtein.get(file, file_name) <= 3);
 
   if (suggestion) {
